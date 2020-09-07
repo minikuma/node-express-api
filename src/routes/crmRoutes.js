1072f507cdbc33f5de1df58c22276db8626d26df
minikuma <minikuma@naver.com>
2020-09-07 17:34:51 +0900

    ver.0.0.2 modified

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,5 +1,5 @@
 /**
- * Created by wminikumagmail.com on 2020/09/07
+ * Created by wminikuma@gmail.com on 2020/09/07
  * Blog : https://minikuma-laboratory.tistory.com/
  * Github : http://github.com/minikuma
  */
